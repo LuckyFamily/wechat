d71f2aecdcd025cbacdf3ac80635cff8055d25b8
JIANGYIMING <jiangyiming@JIANGYIMINGs-MacBook-Pro.local>
2016-05-15 18:59:44 +0800

    add upload function

---
+++ src/com/wechat/test/wechatTest.java
@@ -0,0 +1,15 @@
+package com.wechat.test;
+
+import com.wechat.po.AccessToken;
+import com.wechat.util.wechatUtil;
+
+/**
+ * Created by jiangyiming on 5/15/16.
+ */
+public class wechatTest {
+    public static void main(String[] args) {
+        AccessToken token = wechatUtil.getAccessToken();
+        System.out.println("票据"+token.getToken());
+        System.out.println("有效时间"+token.getExpires_in());
+    }
+}
--- src/com/wechat/util/wechatUtil.java
+++ src/com/wechat/util/wechatUtil.java
@@ -1,5 +1,6 @@
 package com.wechat.util;
 
+import com.wechat.po.AccessToken;
 import net.sf.json.JSONObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -9,7 +10,12 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 
 /**
  * Created by jiangyiming on 5/14/16.
@@ -17,6 +23,8 @@ import java.io.IOException;
 public class wechatUtil {
     private static final String APPID="wxbb3f930754feb0a9";
     private static final String APPSECRET="3236e78bf4a4b01d35463f44c7e74b9a";
+    private static final String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+    private static final String UPLOAD_URL="https://api.weixin.qq.com/cgi-bin/media/upload?access_token=ACCESS_TOKEN&type=TYPE";
 
     /**
      * get请求
@@ -60,5 +68,110 @@ public class wechatUtil {
             e.printStackTrace();
         }
         return jsonObject;
+    }
+
+    /**
+     * 获取accessToken
+     * @param
+     * @param
+     * @return
+     */
+    public static AccessToken getAccessToken(){
+        AccessToken accessToken = new AccessToken();
+        String url = ACCESS_TOKEN_URL.replace("APPID",APPID).replace("APPSECRET",APPSECRET);
+        JSONObject jsonObject = doGetStr(url);
+        if (jsonObject!=null){
+            accessToken.setToken(jsonObject.getString("access_token"));
+            accessToken.setExpires_in(jsonObject.getInt("expires_in"));
+        }
+        return accessToken;
+    }
+
+    public static String upload(String filePath, String accessToken,String type) throws IOException, NoSuchAlgorithmException, NoSuchProviderException, KeyManagementException {
+        File file = new File(filePath);
+        if (!file.exists() || !file.isFile()) {
+            throw new IOException("文件不存在");
+        }
+
+        String url = UPLOAD_URL.replace("ACCESS_TOKEN", accessToken).replace("TYPE",type);
+
+        URL urlObj = new URL(url);
+        //连接
+        HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+
+        con.setRequestMethod("POST");
+        con.setDoInput(true);
+        con.setDoOutput(true);
+        con.setUseCaches(false);
+
+        //设置请求头信息
+        con.setRequestProperty("Connection", "Keep-Alive");
+        con.setRequestProperty("Charset", "UTF-8");
+
+        //设置边界
+        String BOUNDARY = "----------" + System.currentTimeMillis();
+        con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("--");
+        sb.append(BOUNDARY);
+        sb.append("\r\n");
+        sb.append("Content-Disposition: form-data;name=\"file\";filename=\"" + file.getName() + "\"\r\n");
+        sb.append("Content-Type:application/octet-stream\r\n\r\n");
+
+        byte[] head = sb.toString().getBytes("utf-8");
+
+        //获得输出流
+        OutputStream out = new DataOutputStream(con.getOutputStream());
+        //输出表头
+        out.write(head);
+
+        //文件正文部分
+        //把文件以流的方式推入到url中
+        DataInputStream in = new DataInputStream(new FileInputStream(file));
+        int bytes = 0;
+        byte[] bufferOut = new byte[1024];
+        while ((bytes = in.read(bufferOut)) != -1) {
+            out.write(bufferOut, 0, bytes);
+        }
+        in.close();
+
+        //结尾部分
+        byte[] foot = ("\r\n--" + BOUNDARY + "--\r\n").getBytes("utf-8");//定义数据分割线
+
+        out.write(foot);
+
+        out.flush();
+        out.close();
+
+        StringBuffer buffer = new StringBuffer();
+        BufferedReader reader = null;
+        String result = null;
+        try {
+            //定义BufferReader输入流来读取URL响应
+            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+            }
+            if (result == null) {
+                result = buffer.toString();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+
+        JSONObject jsonObj = JSONObject.fromObject(result);
+        System.out.println(jsonObj);
+        String typeName = "media_id";
+        if(!"image".equals(type)){
+            typeName = type + "_media_id";
+        }
+        String mediaId = jsonObj.getString(typeName);
+        return mediaId;
     }
 }
